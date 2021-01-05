87a19b85d2df7e05be583f302a1545762818a170
Toan PLQ <quoctoan.tn05@gmail.com>
2021-01-05 18:49:30 +0700

    fix layout

--- src/Components/RightThread.js
+++ src/Components/RightThread.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import './Style/RightThread.css';
 
-const rightThread = () => <div><p>This is right thread</p></div>;
+const rightThread = () => (
+  <div className="rightThread">
+    <p>This is right thread</p>
+  </div>
+);
 
 export default rightThread;
